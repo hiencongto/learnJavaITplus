c1c10ab3d9a54552e51c5feded0d4d4117b861ae
hiencongto <hienahihi111@gmail.com>
2024-06-12 01:23:30 +0700

    bai ve nha buoi 3

---
+++ Buoi3/bvnMain.java
@@ -0,0 +1,60 @@
+package learnJavaITplus.Buoi3;
+
+import java.util.ArrayList;
+import learnJavaITplus.Buoi3.bvnStudent;
+
+public class bvnMain {
+    public static void main(String[] args) {
+        bvnStudent student1 = new bvnStudent("Name1", 18, "Nam", 8);
+        bvnStudent student2 = new bvnStudent("Name2", 19, "Nữ", 9);
+        bvnStudent student3 = new bvnStudent("Name3", 20, "Nam", 10);
+
+        // array student
+        bvnStudent[] students_array = { student1, student2, student3 };
+
+        // element
+        double highestMark = student1.getPoint();
+        String nameStudentHighestMark = student1.getName();
+        int highestAge = student1.getAge();
+        String inforOldestStudent = student1.toString();
+
+        // because array cant change size, so i try to use arraylist
+        // but i still dont know clearly about that
+        ArrayList<String> nameMales = new ArrayList<>();
+
+        for (bvnStudent student_array : students_array) {
+
+            // check highest mark and set nam to nameStudentHighestMark
+            if (student_array.getPoint() > highestMark) {
+                highestMark = student_array.getPoint();
+                nameStudentHighestMark = student_array.getName();
+            }
+
+            // check gender is "Nam", add name to arraylist nameMale
+            if (student_array.getGender().equals("Nam")) {
+                nameMales.add(student_array.getName());
+            }
+
+            // check highest age
+            if (student_array.getAge() > highestAge) {
+                inforOldestStudent = student_array.toString();
+            }
+        }
+
+        // print in order: the highest mark, name student that male, info the oldest
+        // student
+        System.out.println("Diem cao nhat " + nameStudentHighestMark);
+        for (String nameMale : nameMales) {
+            System.out.println("Hoc vien gioi tinh nam: " + nameMale);
+        }
+        System.out.println(inforOldestStudent);
+
+        /*
+         * result:
+         * Diem cao nhat Name3
+         * Hoc vien gioi tinh nam: Name1
+         * Hoc vien gioi tinh nam: Name3
+         * Ten Name3, Tuoi 20, Gioi tinh Nam, Diem so 10.0
+         */
+    }
+}
---
+++ Buoi3/bvnStudent.java
@@ -0,0 +1,59 @@
+package learnJavaITplus.Buoi3;
+
+public class bvnStudent {
+    private String name;
+    private int age;
+    private String gender;
+    private double point;
+
+    public bvnStudent() {
+
+    }
+
+    public bvnStudent(String name, int age, String gender, double point) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.point = point;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public double getPoint() {
+        return point;
+    }
+
+    public void setPoint(double point) {
+        this.point = point;
+    }
+
+    // print all student's informations
+    public String toString() {
+        String infomation = "Ten " + this.name + ", Tuoi " + this.age
+                + ", Gioi tinh " + this.gender + ", Diem so " + this.point;
+
+        return infomation;
+    }
+}
