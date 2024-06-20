42b186612db47cc2356418ecc254fbdc22feaa1a
hiencongto <hienahihi111@gmail.com>
2024-06-21 01:09:46 +0700

    update bvnbuoi5

--- Buoi5/MyPhoneMain.java
+++ Buoi5/MyPhoneMain.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
 
 public class MyPhoneMain {
 
-    public static ArrayList<MyPhone> sortByQuantity(ArrayList<MyPhone> myPhones) {
+    // sort by quantity using bubble sort
+    public static ArrayList<MyPhone> displayByQuantity(ArrayList<MyPhone> myPhones) {
 
         for (int i = 0; i < myPhones.size() - 1; i++) {
             for (int j = 0; j < myPhones.size() - 1 - i; j++) {
@@ -20,20 +21,41 @@ public class MyPhoneMain {
 
     }
 
+    public static void displayByPrice(ArrayList<MyPhone> myPhoneS) {
+        int theHighestPrice = myPhoneS.get(0).getPrice();
+        String infoHighestPrice = myPhoneS.get(0).toString();
+        int theLowestPrice = myPhoneS.get(0).getPrice();
+        String infoLowestPrice = myPhoneS.get(0).toString();
+
+        for (MyPhone myPhone : myPhoneS) {
+            if (myPhone.getPrice() > theHighestPrice) {
+                infoHighestPrice = myPhone.toString();
+            }
+            if (myPhone.getPrice() < theLowestPrice) {
+                infoLowestPrice = myPhone.toString();
+            }
+        }
+
+        System.out.println("San pham gia cao nhat :" + infoHighestPrice);
+        System.out.println("San pham gia thap nhat :" + infoLowestPrice);
+
+    }
+
     public static void main(String[] args) {
 
-        ArrayList<MyPhone> myphones = new ArrayList<>();
+        ArrayList<MyPhone> myPhoneS = new ArrayList<>();
 
-        myphones.add(new MyPhone("iphone15", "apple", 50, 1000));
-        myphones.add(new MyPhone("note24ultra", "samsung", 51, 950));
-        myphones.add(new MyPhone("8X", "honor", 60, 700));
-        myphones.add(new MyPhone("Reno", "oppo", 100, 750));
-        myphones.add(new MyPhone("14ultra", "xiaomi", 200, 800));
+        myPhoneS.add(new MyPhone("iphone15", "apple", 50, 1000));
+        myPhoneS.add(new MyPhone("note24ultra", "samsung", 51, 950));
+        myPhoneS.add(new MyPhone("8X", "honor", 60, 700));
+        myPhoneS.add(new MyPhone("Reno", "oppo", 100, 750));
+        myPhoneS.add(new MyPhone("14ultra", "xiaomi", 200, 800));
 
-        sortByQuantity(myphones);
+        displayByPrice(myPhoneS);
+        displayByQuantity(myPhoneS);
 
-        for (MyPhone myphone : myphones) {
-            System.out.println(myphone.toString());
+        for (MyPhone myPhone : myPhoneS) {
+            System.out.println(myPhone.toString());
         }
 
     }
