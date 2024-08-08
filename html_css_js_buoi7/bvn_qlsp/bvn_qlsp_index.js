fafe67ca40608159a8301e5ead8dc75ba9098697
hiencongto <hienahihi111@gmail.com>
2024-08-08 19:51:09 +0700

    bvn qlsp html js buoi 7

--- html_css_js_buoi7/bvn_qlsp/bvn_qlsp_index.js
+++ html_css_js_buoi7/bvn_qlsp/bvn_qlsp_index.js
@@ -32,12 +32,26 @@ function displayAllPrd() {
     for (let i = 0; i<arrProduct.length; i++) {
         data += "<tr>"
         data += "<td>"+ i + "</td>"
-        data += "<td>"+arrProduct[i] +"</td>"
-        data += "<td><button onclick = deleteProduct("+i+")>Delete</button></td>"
+        data += "<td onclick = 'formEditProductName("+i+")'>"+arrProduct[i]+"</td>"
+        data += "<td><button onclick = 'deleteProduct("+i+")'>Delete</button></td>"
         data += "</tr>"
     }
     data+="</table>"
     document.getElementById("display").innerHTML = data;
+}
+
+function formEditProductName(index) {
+    let data = ""
+    data+="<label><b>Edit Product</b></label><br>"
+    data+="<input type='text' id='newProductName' name='newProductName' value = '"+arrProduct[index]+"' placeholder = '"+arrProduct[index]+"'>"
+    data+="<button type='button' class='btn btn-success' onclick='editProductName("+index+")'>Edit</button>"
+    document.getElementById("display").innerHTML = data;
+}
+
+function editProductName(index) {
+    let newProductName = document.getElementById("newProductName").value;
+    arrProduct[index] = newProductName;
+    displayAllPrd()
 }
 
 displayAllPrd()
